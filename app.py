0f7436f80da2797f488e3c9d62cc1ac0974789ac
Frederico Cunha <cunha.frederico@gmail.com>
2025-12-10 18:57:16 -0300

    Fix indentation for task deletion response
    Signed-off-by: Frederico Cunha <cunha.frederico@gmail.com>

--- app.py
+++ app.py
@@ -61,11 +61,11 @@ def delete_task(id):
       task = t
       break
 
-    if not task:
-      return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+  if not task:
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
     
-    tasks.remove(task)
-    return jsonify({"message": "Tarefa deletada com sucesso"})
+  tasks.remove(task)
+  return jsonify({"message": "Tarefa deletada com sucesso"})
 
 if __name__ == "__main__":
   app.run(debug=True)
